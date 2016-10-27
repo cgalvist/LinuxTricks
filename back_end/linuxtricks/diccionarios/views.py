c91c342f9fe4504ce90a22333f01aae788a0d0f1
cesar galvis <cgalvist@gmail.com>
2016-10-26 22:45:12 -0500

    servicios provisionales de lista de diccionarios y programas funcionando, falta convertirlos a json

--- back_end/linuxtricks/diccionarios/views.py
+++ back_end/linuxtricks/diccionarios/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from .models import diccionario
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("indice de diccionarios")
+    lista = diccionario.objects.order_by('-diccionario_nombre')
+    output = ', '.join([q.diccionario_nombre for q in lista])
+    return HttpResponse(output)
--- back_end/linuxtricks/programas/views.py
+++ back_end/linuxtricks/programas/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from .models import programa
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("indice de programas")
+    lista = programa.objects.order_by('-programa_nombre')
+    output = ', '.join([q.programa_nombre for q in lista])
+    return HttpResponse(output)
+
