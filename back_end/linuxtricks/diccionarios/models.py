1336f3907645312e775aa6960b8a5e27048a19c9
cesar galvis <cgalvist@gmail.com>
2016-11-26 15:22:30 -0500

    generador de scripts funcionando correctamente y realizadas algunas mejoras en el backend

--- back_end/linuxtricks/diccionarios/models.py
+++ back_end/linuxtricks/diccionarios/models.py
@@ -6,22 +6,22 @@ from django.db import models
 
 class diccionario(models.Model):
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=150)
     logo = models.CharField(max_length=50)
 
 class comando(models.Model):
     diccionario = models.ForeignKey(diccionario, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=200)
     sintaxis = models.CharField(max_length=50)
 
 class opcion(models.Model):
     comando = models.ForeignKey(comando, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=200)
 
 class ejemplo(models.Model):
     comando = models.ForeignKey(comando, on_delete=models.CASCADE)
     entrada = models.CharField(max_length=50)
     salida = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=200)
--- back_end/linuxtricks/programas/models.py
+++ back_end/linuxtricks/programas/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class programa(models.Model):
     nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=150)
     logo = models.CharField(max_length=50)
 
 class categoria(models.Model):
@@ -16,4 +16,4 @@ class atajo(models.Model):
     programa = models.ForeignKey(programa)
     categoria = models.ForeignKey(categoria)
     descripcion = models.CharField(max_length=50)
-    combinacion = models.CharField(max_length=100)
+    combinacion = models.CharField(max_length=200)
